25b948e7e8134cea83ee3fd8b96477059b83dcff
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-02-07 14:47:37 +0100

    SASS added, Player 2 dice changed added, Winner On sides and fisrt scripts into index.js

---
+++ js/index.js
@@ -0,0 +1,47 @@
+document.getElementById("btn-pips-1").addEventListener("click", function() {
+
+  let pips = document.querySelectorAll("#player-1 #pip");
+
+  let pip = 0;
+  if (pips[pip].classList == "dot") {
+
+    while (pip < pips.length) {
+
+      pips[pip].classList.add("rombus");
+      pips[pip].classList.remove("dot");
+      pip++;
+    }
+  } else if (pips[pip].classList == "rombus") {
+
+    while (pip < pips.length) {
+
+      pips[pip].classList.add("dot");
+      pips[pip].classList.remove("rombus");
+      pip++;
+    }
+  }
+});
+
+document.getElementById("btn-pips-2").addEventListener("click", function() {
+
+  let pips = document.querySelectorAll("#player-2 #pip");
+
+  let pip = 0;
+  if (pips[pip].classList == "dot") {
+
+    while (pip < pips.length) {
+
+      pips[pip].classList.add("rombus");
+      pips[pip].classList.remove("dot");
+      pip++;
+    }
+  } else if (pips[pip].classList == "rombus") {
+
+    while (pip < pips.length) {
+
+      pips[pip].classList.add("dot");
+      pips[pip].classList.remove("rombus");
+      pip++;
+    }
+  }
+});
